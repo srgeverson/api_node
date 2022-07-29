69e5a84eb5f225d082cc7afc27fadc2ed94807b5
Geverson <geversonjosedesouza@gmail.com>
2022-07-29 19:14:16 -0300

    Ajuste na prmissão do usuário

--- src/domain/model/Permissao.js
+++ src/domain/model/Permissao.js
@@ -22,7 +22,7 @@ class Permissao extends Model {
     static associate(models) {
         this.belongsToMany(models.Usuario, {
             through: 'usuarios_permissoes',
-            foreignKey: 'usuarioId',
+            foreignKey: 'permissaoId',
             as: 'usuarios'
         });
     }

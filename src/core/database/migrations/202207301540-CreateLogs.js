4a4875d45aa6ec66d97d0a7bd1fff2f722f6b2b8
Geverson <geversonjosedesouza@gmail.com>
2022-07-30 15:54:24 -0300

    Migration de logs

---
+++ src/core/database/migrations/202207301540-CreateLogs.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('logs', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      tabela_nome: {
+        type: Sequelize.STRING(50),
+      },
+      tabela_coluna: {
+        type: Sequelize.STRING(100),
+      },
+      tabela_id: {
+        type: Sequelize.INTEGER
+      },
+      operacao: {
+        type: Sequelize.STRING(6),
+      },
+      data_operacao: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      valor_anterior: {
+        type: Sequelize.STRING(10),
+      },
+      valor_atual: {
+        type: Sequelize.TEXT,
+      },
+      usuarios_id: {
+        type: Sequelize.TEXT
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.dropTable('logs')
+  }
+};
---
+++ src/domain/model/Log.js
@@ -0,0 +1,36 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Log extends Model {
+
+    static init(sequelize) {
+        super.init(
+            {
+                id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, },
+                tabela_nome: { type: Sequelize.STRING(50) },
+                tabela_coluna: { type: Sequelize.STRING(100) },
+                tabela_id: { type: Sequelize.INTEGER },
+                operacao: { type: Sequelize.STRING(6) },
+                data_operacao: { type: "TIMESTAMP", defaultValue: Sequelize.literal("CURRENT_TIMESTAMP") },
+                valor_anterior: { type: Sequelize.STRING(10) },
+                valor_atual: { type: Sequelize.TEXT },
+                usuarios_id: { type: Sequelize.TEXT }
+            },
+            {
+                sequelize,
+                tableName: 'logs'
+            }
+        );
+
+        return this;
+    }
+
+    // static associate(models) {
+    //     this.belongsToMany(models.Usuario, {
+    //         through: 'usuarios_permissoes',
+    //         foreignKey: 'LogId',
+    //         as: 'usuarios'
+    //     });
+    // }
+}
+
+export default Log

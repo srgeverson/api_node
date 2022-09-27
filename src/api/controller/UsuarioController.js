a6de1a4b9650223bb6cfaa100d11094835514007
Geverson <geversonjosedesouza@gmail.com>
2022-09-26 22:31:00 -0300

    validando as permissões dos usuários

--- src/api/controller/UsuarioController.js
+++ src/api/controller/UsuarioController.js
@@ -1,5 +1,5 @@
 import { StatusCode } from 'status-code-enum';
-import { validateToken } from '../../core/auth';
+import { validateRoles, validateToken } from '../../core/auth';
 import UsuarioService from '../../domain/service/UsuarioService';
 
 class UsuarioController {
@@ -9,13 +9,18 @@ class UsuarioController {
     }
 
     async alterarUsuario(request, response) {
-        const { id } = request.params;
-        const { nome, email, ativo } = request.body;
-        const usuarioAlterado = await this.usuarioService.alterarUsuario({ id, nome, email, ativo });
-        if (usuarioAlterado.statusCode)
-            return response.status(usuarioAlterado.statusCode).json(usuarioAlterado);
-        else
-            return response.status(StatusCode.SuccessOK).json(usuarioAlterado);
+        const permissoesValidadas = await validateRoles(['editar_usuario'], request.usuarioAutenticadoRoles);
+        if (permissoesValidadas && permissoesValidadas.statusCode)
+            return response.status(permissoesValidadas.statusCode).json(permissoesValidadas);
+        else {
+            const { id } = request.params;
+            const { nome, email, ativo } = request.body;
+            const usuarioAlterado = await this.usuarioService.alterarUsuario({ id, nome, email, ativo });
+            if (usuarioAlterado.statusCode)
+                return response.status(usuarioAlterado.statusCode).json(usuarioAlterado);
+            else
+                return response.status(StatusCode.SuccessOK).json(usuarioAlterado);
+        }
     }
 
     async alterarFotoUsuario(request, response) {
@@ -28,32 +33,46 @@ class UsuarioController {
             return response.status(StatusCode.SuccessOK).json(usuarioFotoAlterada);
     }
 
-
     async ativarUsuario(request, response) {
-        const { id } = request.params;
-        const usuarioAtivado = await this.usuarioService.ativarOuDesativarUsuario({ id, ativo: true });
-        if (usuarioAtivado.statusCode)
-            return response.status(usuarioAtivado.statusCode).json(usuarioAtivado);
-        else
-            return response.status(StatusCode.SuccessNoContent).json(usuarioAtivado);
+        const permissoesValidadas = await validateRoles(['editar_usuario'], request.usuarioAutenticadoRoles);
+        if (permissoesValidadas && permissoesValidadas.statusCode)
+            return response.status(permissoesValidadas.statusCode).json(permissoesValidadas);
+        else {
+            const { id } = request.params;
+            const usuarioAtivado = await this.usuarioService.ativarOuDesativarUsuario({ id, ativo: true });
+            if (usuarioAtivado.statusCode)
+                return response.status(usuarioAtivado.statusCode).json(usuarioAtivado);
+            else
+                return response.status(StatusCode.SuccessNoContent).json(usuarioAtivado);
+        }
     }
 
     async buscarUsuarioPorId(request, response) {
-        const { id } = request.params;
-        const usuarioEncontrado = await this.usuarioService.buscarPorId(id);
-        if (usuarioEncontrado.statusCode)
-            return response.status(usuarioEncontrado.statusCode).json(usuarioEncontrado);
-        else
-            return response.status(StatusCode.SuccessOK).json(usuarioEncontrado);
+        const permissoesValidadas = await validateRoles(['listar_usuario'], request.usuarioAutenticadoRoles);
+        if (permissoesValidadas && permissoesValidadas.statusCode)
+            return response.status(permissoesValidadas.statusCode).json(permissoesValidadas);
+        else {
+            const { id } = request.params;
+            const usuarioEncontrado = await this.usuarioService.buscarPorId(id);
+            if (usuarioEncontrado.statusCode)
+                return response.status(usuarioEncontrado.statusCode).json(usuarioEncontrado);
+            else
+                return response.status(StatusCode.SuccessOK).json(usuarioEncontrado);
+        }
     }
 
     async buscarUsuarioPorNome(request, response) {
-        const { nome } = request.params;
-        const usuarioEncontrado = await this.usuarioService.buscarPorNome(nome);
-        if (usuarioEncontrado.statusCode)
-            return response.status(usuarioEncontrado.statusCode).json(usuarioEncontrado);
-        else
-            return response.status(StatusCode.SuccessOK).json(usuarioEncontrado);
+        const permissoesValidadas = await validateRoles(['listar_usuario'], request.usuarioAutenticadoRoles);
+        if (permissoesValidadas && permissoesValidadas.statusCode)
+            return response.status(permissoesValidadas.statusCode).json(permissoesValidadas);
+        else {
+            const { nome } = request.params;
+            const usuarioEncontrado = await this.usuarioService.buscarPorNome(nome);
+            if (usuarioEncontrado.statusCode)
+                return response.status(usuarioEncontrado.statusCode).json(usuarioEncontrado);
+            else
+                return response.status(StatusCode.SuccessOK).json(usuarioEncontrado);
+        }
     }
 
     async cadastrarSenhaComCodigo(request, response) {
@@ -66,12 +85,17 @@ class UsuarioController {
     }
 
     async cadastrarUsuarioComSenha(request, response) {
-        const { nome, email, senha } = request.body;
-        const usuarioCadastrado = await this.usuarioService.cadastrarUsuarioComSenha({ nome, email, senha });
-        if (usuarioCadastrado.statusCode)
-            return response.status(usuarioCadastrado.statusCode).json(usuarioCadastrado);
-        else
-            return response.status(StatusCode.SuccessCreated).json(usuarioCadastrado);
+        const permissoesValidadas = await validateRoles(['cadastrar_usuario'], request.usuarioAutenticadoRoles);
+        if (permissoesValidadas && permissoesValidadas.statusCode)
+            return response.status(permissoesValidadas.statusCode).json(permissoesValidadas);
+        else {
+            const { nome, email, senha } = request.body;
+            const usuarioCadastrado = await this.usuarioService.cadastrarUsuarioComSenha({ nome, email, senha });
+            if (usuarioCadastrado.statusCode)
+                return response.status(usuarioCadastrado.statusCode).json(usuarioCadastrado);
+            else
+                return response.status(StatusCode.SuccessCreated).json(usuarioCadastrado);
+        }
     }
 
     async cadastrarUsuarioSemSenha(request, response) {
@@ -84,12 +108,17 @@ class UsuarioController {
     }
 
     async desativarUsuario(request, response) {
-        const { id } = request.params;
-        const usuarioDesativado = await this.usuarioService.ativarOuDesativarUsuario({ id, ativo: false });
-        if (usuarioDesativado.statusCode)
-            return response.status(usuarioDesativado.statusCode).json(usuarioDesativado);
-        else
-            return response.status(StatusCode.SuccessNoContent).json(usuarioDesativado);
+        const permissoesValidadas = await validateRoles(['excluir_usuario'], request.usuarioAutenticadoRoles);
+        if (permissoesValidadas && permissoesValidadas.statusCode)
+            return response.status(permissoesValidadas.statusCode).json(permissoesValidadas);
+        else {
+            const { id } = request.params;
+            const usuarioDesativado = await this.usuarioService.ativarOuDesativarUsuario({ id, ativo: false });
+            if (usuarioDesativado.statusCode)
+                return response.status(usuarioDesativado.statusCode).json(usuarioDesativado);
+            else
+                return response.status(StatusCode.SuccessNoContent).json(usuarioDesativado);
+        }
     }
 
     async enviarCodigoAcessoParaEmail(request, response) {
@@ -102,14 +131,19 @@ class UsuarioController {
     }
 
     async incluirPermissoesAoUsuario(request, response) {
-        const { id } = request.params;
-        const permissoes = request.body;
-        console.log(permissoes);
-        const usuarioAutorizado = await this.usuarioService.incluirPermissoesAoUsuario({ id, permissoes });
-        if (usuarioAutorizado.statusCode)
-            return response.status(usuarioAutorizado.statusCode).json(usuarioAutorizado);
-        else
-            return response.status(StatusCode.SuccessOK).json(usuarioAutorizado);
+        const permissoesValidadas = await validateRoles(['editar_usuario'], request.usuarioAutenticadoRoles);
+        if (permissoesValidadas && permissoesValidadas.statusCode)
+            return response.status(permissoesValidadas.statusCode).json(permissoesValidadas);
+        else {
+            const { id } = request.params;
+            const permissoes = request.body;
+            console.log(permissoes);
+            const usuarioAutorizado = await this.usuarioService.incluirPermissoesAoUsuario({ id, permissoes });
+            if (usuarioAutorizado.statusCode)
+                return response.status(usuarioAutorizado.statusCode).json(usuarioAutorizado);
+            else
+                return response.status(StatusCode.SuccessOK).json(usuarioAutorizado);
+        }
     }
 
     async login(request, response) {
@@ -121,11 +155,14 @@ class UsuarioController {
                 break;
             case 'refresh_token':
                 const refreshToken = await validateToken(refresh_token);
-                usuarioAutorizado = await this.usuarioService.revalidarAcesso({ email: refreshToken.email });
+                if (refreshToken.statusCode)
+                    usuarioAutorizado = refreshToken;
+                else
+                    usuarioAutorizado = await this.usuarioService.revalidarAcesso({ email: refreshToken.email });
                 break;
             default:
                 usuarioAutorizado = await this.usuarioService.validarAcesso({ email, senha });
-                break
+                break;
         }
         if (usuarioAutorizado.statusCode)
             return response.status(usuarioAutorizado.statusCode).json(usuarioAutorizado);
@@ -134,20 +171,30 @@ class UsuarioController {
     }
 
     async todasPermissoesDoUsuario(request, response) {
-        const { id } = request.params;
-        const permissoesDoUsuario = await this.usuarioService.todasPermissoesDoUsuario({ id });
-        if (permissoesDoUsuario.statusCode)
-            return response.status(permissoesDoUsuario.statusCode).json(permissoesDoUsuario);
-        else
-            return response.status(StatusCode.SuccessOK).json(permissoesDoUsuario);
+        const permissoesValidadas = await validateRoles(['listar_usuario'], request.usuarioAutenticadoRoles);
+        if (permissoesValidadas && permissoesValidadas.statusCode)
+            return response.status(permissoesValidadas.statusCode).json(permissoesValidadas);
+        else {
+            const { id } = request.params;
+            const permissoesDoUsuario = await this.usuarioService.todasPermissoesDoUsuario({ id });
+            if (permissoesDoUsuario.statusCode)
+                return response.status(permissoesDoUsuario.statusCode).json(permissoesDoUsuario);
+            else
+                return response.status(StatusCode.SuccessOK).json(permissoesDoUsuario);
+        }
     }
 
     async todosUsuarios(request, response) {
-        const usuarios = await this.usuarioService.buscarTodos();
-        if (usuarios.statusCode)
-            return response.status(usuarios.statusCode).json(usuarios);
-        else
-            return response.status(StatusCode.SuccessOK).json(usuarios);
+        const permissoesValidadas = await validateRoles(['listar_usuario'], request.usuarioAutenticadoRoles);
+        if (permissoesValidadas && permissoesValidadas.statusCode)
+            return response.status(permissoesValidadas.statusCode).json(permissoesValidadas);
+        else {
+            const usuarios = await this.usuarioService.buscarTodos();
+            if (usuarios.statusCode)
+                return response.status(usuarios.statusCode).json(usuarios);
+            else
+                return response.status(StatusCode.SuccessOK).json(usuarios);
+        }
     }
 
 }
--- src/core/auth/index.js
+++ src/core/auth/index.js
@@ -2,46 +2,6 @@ import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 import { StatusCode } from 'status-code-enum';
 
-const resourceOwner = async (request, response, next) => {
-    try {
-        const chave = process.env.KEY_SECRET;
-
-        const authorizationHeader = request.headers.authorization;
-
-        console.log(authorizationHeader);
-
-        if (!authorizationHeader)
-            return response.status(StatusCode.ClientErrorUnauthorized).json({
-                statusCode: StatusCode.ClientErrorUnauthorized,
-                message: 'Token não informado!'
-            });
-
-        const [bearer, token] = authorizationHeader.split(' ');
-
-        const decoded = await promisify(jwt.verify)(token, chave);
-        request.usuarioAutenticadoId = decoded.id;
-        
-        return next();
-    } catch (err) {
-        return response.status(StatusCode.ClientErrorUnauthorized).json({
-            statusCode: StatusCode.ClientErrorUnauthorized,
-            message: 'Token inválido!'
-        });
-    }
-}
-
-const validateToken = async (token) => {
-    try {
-        const chave = process.env.KEY_SECRET;
-        return await promisify(jwt.verify)(token, chave);
-    } catch (err) {
-        return response.status(StatusCode.ClientErrorUnauthorized).json({
-            statusCode: StatusCode.ClientErrorUnauthorized,
-            message: 'Token inválido!'
-        });
-    }
-}
-
 const client = async (request, response, next) => {
     try {
         const client_id = process.env.CLIENT_ID;
@@ -78,4 +38,56 @@ const client = async (request, response, next) => {
     }
 }
 
-export { resourceOwner, client,validateToken };
+const resourceOwner = async (request, response, next) => {
+    try {
+        const chave = process.env.KEY_SECRET;
+
+        const authorizationHeader = request.headers.authorization;
+
+        console.log(authorizationHeader);
+
+        if (!authorizationHeader)
+            return response.status(StatusCode.ClientErrorUnauthorized).json({
+                statusCode: StatusCode.ClientErrorUnauthorized,
+                message: 'Token não informado!'
+            });
+
+        const [bearer, token] = authorizationHeader.split(' ');
+
+        const {id, roles} = await promisify(jwt.verify)(token, chave);
+        request.usuarioAutenticadoId = id;
+        request.usuarioAutenticadoRoles = roles;
+
+        return next();
+    } catch (err) {
+        return response.status(StatusCode.ClientErrorUnauthorized).json({
+            statusCode: StatusCode.ClientErrorUnauthorized,
+            message: 'Token inválido!'
+        });
+    }
+}
+
+const validateToken = async (token) => {
+    try {
+        const chave = process.env.KEY_SECRET;
+        return await promisify(jwt.verify)(token, chave);
+    } catch (err) {
+        return {
+            statusCode: StatusCode.ClientErrorUnauthorized,
+            message: 'Token inválido!'
+        };
+    }
+}
+
+const validateRoles = async (rolesEndpoint = [], rolesToken = [], endPoint = null) => {
+    let roles = [];
+    rolesEndpoint.forEach(item => (rolesToken.indexOf(item) === -1) && roles.push(item));
+
+    if (roles && roles.length)
+        return {
+            statusCode: StatusCode.ClientErrorForbidden,
+            message: `${endPoint ? 'Endpoint de origem ' + endPoint + '.' : ''} Acesso negado, solicitação exige a(s) permissão(ões) [${roles}].`
+        };
+}
+
+export { client, resourceOwner, validateRoles, validateToken };
